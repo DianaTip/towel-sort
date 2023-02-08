6e875df64d7cc643aeb17a2de386010ecaaf28e4
Diana <dianatipunova@gmail.com>
2023-02-08 10:25:47 +0300

    feat: add js file

--- src/index.js
+++ src/index.js
@@ -3,14 +3,17 @@
 
 module.exports = function towelSort (matrix) {
  var matrix1 = []
-  for (var i = 0; i < matrix.length; i++)
+if (arguments.length == 0)
+  return matrix1; 
+  else
+  {for (var i = 0; i < matrix.length; i++)
    {   if (i%2 !== 0)
       { matrix[i].reverse();}
-    
-      for (var j = 0; j < matrix.length; j++)
+      for (var j = 0; j < matrix[i].length; j++)
+ 
        {matrix1.push(matrix[i][j]);
        } 
       }
+      return matrix1
+}}
 
-  return [matrix1];
-}
